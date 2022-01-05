8c0e79dfcbce8dae82a7265513e140b4cd95af0c
hassandaru <hassandaru@gmail.com>
2022-01-05 18:34:35 +0930

    initial setup

---
+++ Mosque Organiser/Mosque_OrganiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mosque_OrganiserApp.swift
+//  Mosque Organiser
+//
+//  Created by Hassan Sohail Dar on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Mosque_OrganiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mosque OrganiserTests/Mosque_OrganiserTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mosque_OrganiserTests.swift
+//  Mosque OrganiserTests
+//
+//  Created by Hassan Sohail Dar on 5/1/22.
+//
+
+import XCTest
+@testable import Mosque_Organiser
+
+class Mosque_OrganiserTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
